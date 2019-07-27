2a6ee3b1fe27744050b601ad8422497ae36a8c60
cyber2611 <52934500+cyber2611@users.noreply.github.com>
2019-07-28 02:45:47 +0700

    Add files via upload

---
+++ Hacker26.sh
@@ -0,0 +1,127 @@
+!#/bin/bash
+clear
+figlet  Brother ID | lolcat
+
+
+
+
+echo "
+[≠]=================================================================[≠]
+[≠]  * Author  : Brother                                          [≠]
+[≠]  * Support : Brother VictoryHacker[] / [!]JANGAN DIJUAL / BV [≠]
+[≠]  * GitHub  : https://github.com/cyber2611                     [≠]
+[≠]  *Decompiled by Yoga Wira                                       [≠]
+[≠]=================================================================[≠]"
+echo
+echo "TEKAN CTRL + Z JIKA MAU EXIT" | lolcat
+echo "{•}=========================================={•}" | lolcat
+echo "   {1} DARK_FACEBOOK Diamond1....               " | lolcat
+echo "   {2} DARK_FACEBOOK Mod....               " | lolcat
+echo "   {3} DARK_FACEBOOK Dark-Naga....       | lolcat      "
+echo "   {4} DARK_FACEBOOK Pro.....              " | lolcat
+echo "   {5} INSTALL BAHAN.....                   "
+echo "{•}=========================================={•}" | lolcat
+echo | lolcat
+read -p "Masukan Pilihan Anda Cok : " aku
+
+if [ $aku = 1 ] || [ $aku = 1 ]
+then
+clear
+echo "█████████
+█▄█████▄█      ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+█▼▼▼▼▼ - _ --_--╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗
+█  _-_-- -_ --__ ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
+█▲▲▲▲▲--  - _ --═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝ Diamond1
+█████████      «----------✧----------»
+ ██ ██" | lolcat
+sleep 3
+$ pkg install python2
+$ pip2 install mechanize
+$ pip2 install requests
+$ pkg install git
+$ git clone https://github.com/wira2611/diamon
+$ ls
+$ cd diamon
+$ ls
+$ python2 diamond-1.py
+fi
+
+if [ $aku = 2 ]  || [ $aku = 02 ]
+then
+clear
+echo "█████████
+█▄█████▄█      ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+█▼▼▼▼▼ - _ --_--╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗
+█  _-_-- -_ --__ ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
+█▲▲▲▲▲--  - _ --═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝ Mod
+█████████      «----------✧----------»
+ ██ ██" | lolcat
+$ pkg install python2
+$ pip2 install requests
+$ pip2 install mechanize
+$ pkg install git
+$ git clone https://github.com/cyber2611/Cyber26-Mod
+$ ls
+$ cd Cyber26-Mod
+$ ls
+$ python2 Dark-Mod.py
+fi
+
+if [ $aku = 3 ] || [ $aku = 03 ]
+then
+clear
+echo "█████████
+█▄█████▄█      ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+█▼▼▼▼▼ - _ --_--╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗
+█  _-_-- -_ --__ ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
+█▲▲▲▲▲--  - _ --═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝ Dark-Naga
+█████████      «----------✧----------»
+ ██ ██" | lolcat
+$ pkg update
+$ pkg upgrade
+$ pkg install python2
+$ pip2 install requests
+$ pip2 install mechanize
+$ pkg install git
+$ git clone https://github.com/cyber2611/Drak-Naga
+$ ls
+$ cd Drak-Naga
+$ ls
+$ python2 Dark-Naga.py
+fi
+
+if [ $aku = 4 ] || [ $aku = 04 ]
+then
+clear
+echo "█████████
+█▄█████▄█      ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+█▼▼▼▼▼ - _ --_--╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗
+█  _-_-- -_ --__ ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
+█▲▲▲▲▲--  - _ --═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝ Pro
+█████████      «----------✧----------»
+ ██ ██" | lolcat
+$ pkg update
+$ pkg upgrade
+$ pkg install python2
+$ pip2 install mechanize
+$ pip2 install requests
+$ pkg install git
+$ git clone https://github.com/wira2611/Pro2611
+$ ls
+$ cd Pro2611
+$ python2 Pro.py
+fi
+
+if [ $aku = 5 ] || [ $aku = 05 ]
+then
+clear
+$ pkg install python2
+$ pip2 install mechanize
+$ pip2 install requests
+$ pkg install git
+$ git clone https://github.com/cyber2611/Hacker11
+$ ls
+$ cd Hacker11
+$ ls
+$ sh hacker26.sh
+fi
